4d5654136bc2171e48e3030613100e7e4f76b047
Suhas-098 <suhasmagadum05@gmail.com>
2025-11-08 13:08:00 +0530

    login&logout points are created

--- Back-End/routes/auth.routes.js
+++ Back-End/routes/auth.routes.js
@@ -60,20 +60,56 @@ router.post("/signup", async (req, res) => {
 
 
 
-
-
-
-
-
-
 // ------- LOGIN ROUTE --------
-router.post("/login", (req, res) => {
-  res.send("login endpoint");
+router.post("/login", async(req, res) => {
+  const{Email,Password}=req.body
+  try{
+    const user=await User.findOne({Email});
+    if(!user) return res.status(400).json({message:"Invalid Credentials"});
+      
+    const isPasswordCorrect=await bcrypt.compare(Password,user.Password)
+    if(!isPasswordCorrect) return res.status(400).json({message:"Invalid Credentials"});
+
+    generateToken(user._id,res)
+    res.status(200).json({
+      _id:user._id,
+      FullName:user.FullName,
+      Email:user.Email,
+      Profilepic:user.Profilepic,
+    });
+  }
+ catch (error) {
+    console.error("Error in login controller:",error);
+    return res.status(500).json({ message: "Server error" });
+  }
+            
 });
+
+
 
 // ------- LOGOUT ROUTE --------
-router.post("/logout", (req, res) => {
-  res.send("logout endpoint");
+router.post("/logout", (_,res) => {
+res.cookie("jwt","",{maxAge:0});
+res.status(200).json({ message: "Logged out successfully" });
+
+  
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default router;
